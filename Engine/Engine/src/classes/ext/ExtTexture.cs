6f3b61edc1830175c8ce3041a8bcb2262946a25f
Roy Tu <thatsnail@gmail.com>
2012-04-12 23:59:06 -0400

    Added ColorTexture()

--- Engine/Engine/src/classes/ext/ExtTexture.cs
+++ Engine/Engine/src/classes/ext/ExtTexture.cs
@@ -18,5 +18,17 @@ namespace Engine
             tex.SetData<Color>(col);
             return tex;
         }
+        public static Texture2D ColorTexture(Texture2D src, Color color)
+        {
+            Texture2D tex = new Texture2D(src.GraphicsDevice, src.Width, src.Height);
+            Color[] col = new Color[src.Width * src.Height];
+            tex.GetData<Color>(col);
+            for (uint i = 0; i < col.Length; i++)
+            {
+                col[i] = Color.White * col[i].A;
+            }
+            tex.SetData<Color>(col);
+            return tex;
+        }
     }
 }
