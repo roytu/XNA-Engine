b54aec946f234b9a7637f2fd7a23aca10ad10bff
Roy Tu <thatsnail@gmail.com>
2012-04-12 22:21:23 -0400

    Added ExtTexture.cs

---
+++ Engine/Engine/src/classes/ext/ExtTexture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Engine
+{
+    class ExtTexture
+    {
+        public static Texture2D CopyTexture(Texture2D src)
+        {
+            Texture2D tex=new Texture2D(src.GraphicsDevice,src.Width,src.Height);
+            Color[] col=new Color[src.Width*src.Height];
+            src.GetData<Color>(col);
+            tex.SetData<Color>(col);
+            return tex;
+        }
+    }
+}
