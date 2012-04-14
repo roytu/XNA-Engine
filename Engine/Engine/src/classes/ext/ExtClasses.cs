6b753aaf1a3eb217badca20bfa9cda686fd373e4
Roy Tu <thatsnail@gmail.com>
2012-04-14 11:03:35 -0400

    ExtClasses.cs added

---
+++ Engine/Engine/src/classes/ext/ExtClasses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    class ExtClasses
+    {
+        public class Grid<T>
+        {
+            private List<List<T>> grid;
+            public uint width;
+            public uint height;
+
+            public Grid(uint width, uint height, T def)
+            {
+                this.width = width;
+                this.height = height;
+                for (uint x = 0; x < width; x++)
+                {
+                    List<T> ls = new List<T>();
+                    for (uint y = 0; y < height; y++)
+                    {
+                        ls.Add(def);
+                    }
+                    grid.Add(ls);
+                }
+            }
+
+            public T Get(int x, int y)
+            {
+                return grid[x][y];
+            }
+            public void Set(int x, int y, T value)
+            {
+                grid[x][y] = value;
+            }
+        }
+    }
+}
